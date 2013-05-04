b656eca04fa123861af815c253b7c072f4f591e1
reeedstudio <luweicong@qq.com>
2013-05-04 09:28:57 +0800

    test ok

--- Cloud_Dragrove_Test.ino
+++ Cloud_Dragrove_Test.ino
@@ -4,15 +4,47 @@
 #include <SoftwareSerial.h>
 
 #define START1      0x53
-#define START2      0x15
+#define START2      0x12
 #define END1        0x2f
 #define END2        0x45
+
+#define DATATYPEATOMADD	0X01					// add an atom device
+#define DATATYPEATOMDEL	0X02					// del an atom device
+#define DATATYPEPOST	0X03					// post data to yeelink
 
 SoftwareSerial mySerial(6, 7); // RX, TX
 
 unsigned char dtaUart[20];
 unsigned char dtaLen = 0;
 unsigned char getDta = 0;
+
+/*********************************************************************************************************
+** Function name:           sendDtaSerial
+** Descriptions:            send data to serial
+*********************************************************************************************************/
+unsigned char sendDtaSerial(unsigned char type, unsigned char len, unsigned char *dta)
+{
+    unsigned char dtaSerial[20];
+    dtaSerial[0] = START1;
+    dtaSerial[1] = START2;
+    dtaSerial[2] = type;
+    dtaSerial[3] = len;
+    for(int i=0; i<len; i++)
+    {
+        dtaSerial[4+i] = dta[i];
+    }
+    
+    dtaSerial[4+len] = END1;
+    dtaSerial[5+len] = END2;
+    
+    for(int i=0; i<(6+len); i++)
+    {
+        mySerial.write(dtaSerial[i]);
+        Serial.print(dtaSerial[i]);
+        Serial.print('\t');
+    }
+    Serial.println();
+}
 
 /*********************************************************************************************************
 ** Function name:           addDevice
@@ -29,6 +61,9 @@ unsigned char addDevice(unsigned char deviceId, unsigned char sensorId, unsigned
     Serial.println(sensorId);
     Serial.print("actuatorId =\t");
     Serial.println(actuatorId);
+    
+    unsigned char dat[3] = {deviceId, sensorId, actuatorId};
+    sendDtaSerial(DATATYPEATOMADD, 3, dat);
     return 1;
 }
 
@@ -42,6 +77,10 @@ unsigned char delDevice(unsigned char deviceId)
     Serial.println("\r\n----------DEL CMD----------\r\n");
     Serial.print("deviceId =\t");
     Serial.println(deviceId);
+    
+    unsigned char dat[10] = {deviceId};
+    sendDtaSerial(DATATYPEATOMDEL, 1, dat);
+    
     return 1;
 }
 
@@ -52,12 +91,18 @@ unsigned char delDevice(unsigned char deviceId)
 unsigned char pushDta(unsigned char deviceId)
 {
     // add code here 
-    unsigned int dat= random(0, 1023);
+    unsigned int tmp= random(0, 1023);
     Serial.println("\r\n----------PUSH CMD----------\r\n");
     Serial.print("deviceId =\t");
     Serial.println(deviceId);
     Serial.print("data =\t\t");
-    Serial.println(dat);
+    Serial.println(tmp);
+    
+    unsigned char dtaH = tmp>>8;
+    unsigned char dtaL = tmp;
+    
+    unsigned char dat[5] = {deviceId, dtaH, dtaL};
+    sendDtaSerial(DATATYPEPOST, 3, dat);
     return 1;
 }
 
@@ -146,8 +191,8 @@ OK:
 *********************************************************************************************************/
 void setup()
 {
-    Serial.begin(115200);               // hard serial baudrate, you can change it to fit you
-    mySerial.begin(115200);             // you can change baudrate to fit you
+    Serial.begin(9600);               // hard serial baudrate, you can change it to fit you
+    mySerial.begin(9600);             // you can change baudrate to fit you
     
     Serial.println("hello world");      // just to show setup over
 }
